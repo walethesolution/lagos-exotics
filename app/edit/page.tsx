d29be1449909d2e0f6ed21df31d6e6e76639e866
walethesolution <olawaleayejuyone@gmail.com>
2024-01-21 18:34:40 -0500

    Add fragments

--- app/edit/page.tsx
+++ app/edit/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 import React from "react";
 import Layout from "../components/Layout";
+import Admin from "../components/Admin";
 
 const page = () => {
-  return <Layout>{}</Layout>;
+  return (
+    <Layout>
+      <></>
+    </Layout>
+  );
 };
 
 export default page;
