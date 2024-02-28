4a3e245b527fa3e2ec4f0f2de195c9bd486fda62
walethesolution <olawaleayejuyone@gmail.com>
2024-02-27 20:48:04 -0500

    Add suspence to product-form route

--- app/product-form/page.tsx
+++ app/product-form/page.tsx
@@ -1,11 +1,13 @@
-import React from "react";
+import React, { Suspense } from "react";
 import Layout from "@/src/components/Layout";
 import ProductForm from "@/src/components/ProductForm";
 
 const page = () => {
   return (
     <Layout>
-      <ProductForm />
+      <Suspense fallback={<div>Loading...</div>}>
+        <ProductForm />
+      </Suspense>
     </Layout>
   );
 };
